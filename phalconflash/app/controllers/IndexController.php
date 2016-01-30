76b16b4171270b2366e3060d14bb350fe3cda422
Nhan <ngo.nhan.09@gmail.com>
2016-01-30 13:48:32 +0700

    coding for flash demo

--- phalconflash/app/controllers/IndexController.php
+++ phalconflash/app/controllers/IndexController.php
@@ -1,12 +1,12 @@
 <?php
-
-class IndexController extends ControllerBase
-{
-
-    public function indexAction()
-    {
-
-    }
-
+class IndexController extends ControllerBase {
+	public function indexAction() {
+		$this->flash->error ( 'Cần mạng để có thể load bootstrap' );
+		$this->flash->warning( 'Note: Cẩn thận' );
+		$this->flash->success( 'Thành công' );
+		$this->flash->notice( 'Note:information' );
+		// can using flashSession insteand of flash 
+		//$this->flashSession->notice( 'Note:information' );
+	}
 }
 
